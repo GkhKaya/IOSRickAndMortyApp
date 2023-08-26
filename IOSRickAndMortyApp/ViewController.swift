9bd43adff751f0d1255bb9df581e4af49f36212a
GkhKaya <gkhkaya00@gmail.com>
2023-08-26 11:38:31 +0300

    First Commit

---
+++ IOSRickAndMortyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSRickAndMortyApp
+//
+//  Created by Gokhan Kaya on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
